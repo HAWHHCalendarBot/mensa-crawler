85e25ea3972f355c74d5b5349606829bfd803044
EdJoPaTo <edjopato@gmail.com>
2020-01-10 09:43:53 +0100

    feat(network): use user-agent

--- source/crawler/index.ts
+++ source/crawler/index.ts
@@ -1,13 +1,12 @@
-import got from 'got'
-
 import {Canteen, canteenWeekUrl} from '../canteen'
 import {Meal} from '../meal'
+import {request} from '../network'
 import {weekNumber} from '../weeknumber'
 
 import {loadCanteenFromSource, loadMealsFromSource} from './source'
 
 export async function loadCanteens(): Promise<Canteen[]> {
-	const response = await got('http://www.studierendenwerk-hamburg.de/studierendenwerk/de/essen/speiseplaene/')
+	const response = await request('http://www.studierendenwerk-hamburg.de/studierendenwerk/de/essen/speiseplaene/')
 	const canteens = loadCanteenFromSource(response.body)
 	return canteens
 }
@@ -27,7 +26,7 @@ export async function loadMealsOfCanteenCurrentlyKnown(canteen: Canteen): Promis
 
 async function loadMealsOfCanteen(canteen: Canteen, year: number, week: number): Promise<Meal[]> {
 	const url = canteenWeekUrl(canteen.id, year, week)
-	const response = await got(url)
+	const response = await request(url)
 	const meals = loadMealsFromSource(response.body)
 	return meals
 }
