02889ab2beb23f758a8a9384fca21add180a1194
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-04-21 14:34:33 +0200

    refactor: readonly interfaces

--- source/canteen.ts
+++ source/canteen.ts
@@ -1,6 +1,6 @@
 export interface Canteen {
-	id: number;
-	name: string;
+	readonly id: number;
+	readonly name: string;
 }
 
 export function canteenWeekUrl(id: number, year: number, week: number): string {
--- source/meal.ts
+++ source/meal.ts
@@ -1,25 +1,25 @@
 type PriceInEuro = number
 
 export interface MealPrices {
-	PriceAttendant: PriceInEuro;
-	PriceGuest: PriceInEuro;
-	PriceStudent: PriceInEuro;
+	readonly PriceAttendant: PriceInEuro;
+	readonly PriceGuest: PriceInEuro;
+	readonly PriceStudent: PriceInEuro;
 }
 
 export interface MealContents {
-	Alcohol: boolean;
-	Beef: boolean;
-	Fish: boolean;
-	LactoseFree: boolean;
-	Pig: boolean;
-	Poultry: boolean;
-	Vegan: boolean;
-	Vegetarian: boolean;
+	readonly Alcohol: boolean;
+	readonly Beef: boolean;
+	readonly Fish: boolean;
+	readonly LactoseFree: boolean;
+	readonly Pig: boolean;
+	readonly Poultry: boolean;
+	readonly Vegan: boolean;
+	readonly Vegetarian: boolean;
 }
 
 export interface Meal extends MealContents, MealPrices {
-	Name: string;
-	Category: string;
-	Date: string;
-	Additives: Record<string, string>;
+	readonly Name: string;
+	readonly Category: string;
+	readonly Date: string;
+	readonly Additives: Readonly<Record<string, string>>;
 }
